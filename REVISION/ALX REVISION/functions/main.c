5faebf2838a2586032870f5d46237d6d55f1c5ad
Chibundu <ndubest56@gmail.com>
2023-05-07 19:07:34 +0100

    natural number above of below 89 from a givin num

--- REVISION/ALX REVISION/functions/main.c
+++ REVISION/ALX REVISION/functions/main.c
@@ -21,7 +21,7 @@ void print_alphabet_x10()
         }
 }
 //check if its lower of upper
-int islower(char c)
+int _islower(char c)
 {
         if (c >= 'a' && c <= 'z')
                 printf("its lower\n");
@@ -48,6 +48,99 @@ int lastdigit(int n)
 
 }
 
+//prints every minuts of the day
+int everyminute()
+{
+        for (i = 0; i < 24; i++)
+        {
+                for (j = 0; j < 60; j++)
+                {
+                        _putchar((i/10)+'0');
+                        _putchar((i%10)+'0');
+                        _putchar(':');
+                        _putchar((j/10)+'0');
+                        _putchar((j%10)+'0');
+                        _putchar(' ');
+
+                }
+        }
+        _putchar('\n');
+}
+
+//print the 9 X times table
+int times9()
+{
+        for (i = 0; i <= 9; i++)
+        {
+                 for(j = 0; j <= 9; j++)
+                 {
+                         int prod = i * j;
+                         if (j == 0)
+                             putchar('0');
+                         else if (prod <= 9)
+                         printf(",  %d", prod);
+                         else
+                         printf(", %d%d", prod/10, prod%10);
+                 }
+                 putchar('\n');
+        }
+        putchar('\n');
+}
+
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int prod = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			} else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			} else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+//print all natural numbers from given number to 89;
+int printnum(int n)
+{
+        if(n >= 89)
+        {
+                for (i = n; i >= 89; i--)
+                {
+                        printf("%d", i);
+                        if(i != 89)
+                             printf(", ");
+                }
+        }else
+        {
+                for (i = n; i <= 89; i++)
+                {
+                        printf("%d");
+                        if(i != 89)
+                             printf(", ");
+                }
+        }
+}
 //MAIN FUNCTION
 int main(void)
 {
@@ -56,13 +149,21 @@ int main(void)
 
         print_alphabet_x10();
 
-        islower('H');
+        _islower('H');
 
         int n = _abs(-45);
         printf("%d\n", n);
 
         int a = lastdigit(-3036);
         printf("%d\n", a);
+
+        everyminute();
+
+        times9();
+
+        times_table();
+
+        printnum(59);
 
         return (0);
 }
