3b4a450e0f7a6606d4cfa0589190a8d12111a157
Chibundu <ndubest56@gmail.com>
2023-05-07 12:00:29 +0100

    check lower and upper alphabet

--- REVISION/ALX REVISION/functions/main.c
+++ REVISION/ALX REVISION/functions/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int i, j;
+
 //character printing function
 int _putchar(char c)
 {
@@ -11,7 +13,7 @@ int _putchar(char c)
 //print alphabets 10 times
 void print_alphabet_x10()
 {
-        int i, j;
+
 
         for (i = 0; i < 10; i++)
         {
@@ -19,6 +21,15 @@ void print_alphabet_x10()
                     putchar(j);
                 _putchar('\n');
         }
+}
+int islower(char c)
+{
+        if (c >= 'a' && c <= 'z')
+                printf("its lower");
+        else if (c >= 'A' && c <= 'Z')
+                printf("its UPPER");
+        else
+                printf("its not an alphabet");
 }
 
 //MAIN FUNCTION
@@ -28,6 +39,8 @@ int main(void)
         _putchar('\n');
 
         print_alphabet_x10();
-        
+
+        islower('xZv');
+
         return (0);
 }
