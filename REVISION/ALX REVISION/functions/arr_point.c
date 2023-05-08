d5fabab7694ab8fae9c7fce34d1537e5f885099b
Chibundu <ndubest56@gmail.com>
2023-05-08 20:47:46 +0100

    check number of string

--- REVISION/ALX REVISION/functions/arr_point.c
+++ REVISION/ALX REVISION/functions/arr_point.c
@@ -13,13 +13,24 @@ void update(int *n)
 //swap integers
 void swap(int *n, int *m)
 {
-        int *i = n;
-        n = m;
-        m = i;
+        int i = *n;
+        *n = *m;
+        *m = i;
 }
 
+//returns the length of a string
+int strlent(char *s)
+{
+        int i, sum = 0;
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                sum ++;
+        }
+        printf("%d", sum);
+}
 //MAIN FUNCTION
 int main()
 {
         update(100);
+        strlent("hellogoikj");
 }
